5aa4dc58464e197b26ae07c647b8d62a525c3926
JosWambugu <josphatwambugu77@gmail.com>
2021-05-01 12:06:14 +0300

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "Josphatwambugu12",
+  password: "",
   database: "hello_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
